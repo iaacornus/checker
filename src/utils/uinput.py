6421e636516856debd48adf5866e5ce5ea1670d5
James Aaron Erang <iaacornus.devel@gmail.com>
2024-09-12 18:53:51 +0800

    include logging

--- src/utils/uinput.py
+++ src/utils/uinput.py
@@ -1,9 +1,14 @@
 from collections.abc import Callable
 from typing import Any
 
+from src.utils.logger import Logger
+
 
 class ParseInput:
-    def __init__(self, input_func: Callable) -> None:
+    def __init__(
+            self, log: Logger, input_func: Callable
+        ) -> None:
+        self.log = log
         self.input_func: Callable = input_func
 
     def call_func(self) -> dict[str, Any]:
