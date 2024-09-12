234ac6f3e157757dfd3db90c581c676983e35ce4
James Aaron Erang <iaacornus.devel@gmail.com>
2024-09-12 18:42:57 +0800

    source code check

---
+++ src/utils/check.py
@@ -0,0 +1,13 @@
+class Checker:
+    def __init__(self) -> None:
+        pass
+
+    def eval(self) -> float:
+        pass
+
+    def classify(self) -> str:
+        pass
+
+    def check(self) -> bool:
+        return True
+
