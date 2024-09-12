cae22cfc41f884cb4d721909f81f851917225a0a
James Aaron Erang <iaacornus.devel@gmail.com>
2024-09-12 18:48:23 +0800

    initial code

--- src/utils/uinput.py
+++ src/utils/uinput.py
@@ -1,3 +1,14 @@
+from collections.abc import Callable
+from typing import Any
+
+
 class ParseInput:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, input_func: Callable) -> None:
+        self.input_func: Callable = input_func
+
+    def call_func(self) -> dict[str, Any]:
+        return {
+            "any": Any
+        }
+
+
